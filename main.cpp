e02cf30f5398dacf4b4fccc7107e743fb1e21867
Saad-Hussain31 <saadnasir31@gmail.com>
2023-12-31 15:22:29 +0500

    create project hierarchy and link boost mpi

---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <boost/mpi.hpp>
+
+namespace mpi = boost::mpi;
+
+int main(int argc, char* argv[]) {
+    mpi::environment env(argc, argv);
+    mpi::communicator world;
+
+    int rank = world.rank();
+    int size = world.size();
+
+    std::cout << "Hello from rank " << rank << " of " << size << " processes." << std::endl;
+
+    return 0;
+}
